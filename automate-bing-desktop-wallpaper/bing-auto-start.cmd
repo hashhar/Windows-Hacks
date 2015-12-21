7802eb6900f55ca7826802b883592df17845ed94
Ashhar Hasan <hashhar_dev@outlook.com>
2015-12-21 19:00:15 +0530

    Code cleanup

--- automate-bing-desktop-wallpaper/bing-auto-start.cmd
+++ automate-bing-desktop-wallpaper/bing-auto-start.cmd
@@ -1,3 +1,4 @@
+@echo off
 start "Starting Bing Desktop Toolbar" "C:\Program Files (x86)\Microsoft\BingDesktop\BingDesktop.exe"
 timeout /nobreak /t 120
 rem Kill the BingDesktop task
--- save-windows-spotlight-lockscreens/save-spotlight.bat
+++ save-windows-spotlight-lockscreens/save-spotlight.bat
@@ -1,8 +1,5 @@
-rem Create a Temp directory within the target directory passed as the argument to hold copied files
 mkdir %1%\Temp
-rem Go to the directory containing the images
 cd /D %USERPROFILE%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager*\LocalState\Assets\
-rem Copy all the files to the Temp directory we just created (Target directory must not end in slashes)
 for %%e in ("*") do copy /Y %%e %1%\Temp\
 rem Go to the Temp directory to rename all the files to *.jpg
 cd /D %1%\Temp\
@@ -10,6 +7,5 @@ for %%f in ("*") do ren %%f %%f.jpg
 rem Move all the renamed files from the Temp directory to the Target directory
 for %%g in ("*") do move %%g ../
 cd ..
-rem Remove the Temp directory
 rmdir Temp
 rem TODO: Save the files to %USERPROFILE%/Pictures/ if no argument is passed
