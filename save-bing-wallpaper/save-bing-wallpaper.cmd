a72e46a91f3e37c91f391b4bed9f77666aa0f804
Ashhar Hasan <hashhar_dev@outlook.com>
2015-12-23 13:31:25 +0530

    Added ability to save pictures to Pictures folder if not argument passed

--- save-bing-wallpaper/save-bing-wallpaper.cmd
+++ save-bing-wallpaper/save-bing-wallpaper.cmd
@@ -1,4 +1,10 @@
-rem The location is actually %USERPROFILE%\AppData\Local\Microsoft\BingDesktop\locale\Apps\Wallpaper_somerandomhash\VersionIndependent\images where locale is en-us, en-gb etc.
+@echo off
+rem "The location is actually %USERPROFILE%\AppData\Local\Microsoft\BingDesktop\locale\Apps\Wallpaper_somerandomhash\VersionIndependent\images where locale is en-us, en-gb etc."
 cd /D %USERPROFILE%\AppData\Local\Microsoft\BingDesktop\en-us\Apps\Wallpaper_*\VersionIndependent\images\
-rem TODO: Copy to %USERPROFILE%/Pictures/ if no argument is passed
-for %%e in ("*.jpg") do copy /Y %%e %1%\
+@echo on
+if not exist "%USERPROFILE%\Pictures\Bing" mkdir "%USERPROFILE%\Pictures\Bing"
+if [%1]==[] (
+	for %%e in ("*.jpg") do copy /Y %%e %USERPROFILE%\Pictures\Bing\
+) else (
+	for %%e in ("*.jpg") do copy /Y %%e %1%\
+)
--- save-windows-spotlight-lockscreens/save-spotlight.bat
+++ save-windows-spotlight-lockscreens/save-spotlight.bat
@@ -1,11 +1,20 @@
-mkdir %1%\Temp
+@echo off
+if [%1]==[] (
+	set base_path=%USERPROFILE%\Pictures\Spotlight
+) else (
+	set base_path=%1
+)
+if not exist "%base_path%" mkdir "%base_path%"
+mkdir "%base_path%\Temp"
 cd /D %USERPROFILE%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager*\LocalState\Assets\
-for %%e in ("*") do copy /Y %%e %1%\Temp\
-rem Go to the Temp directory to rename all the files to *.jpg
-cd /D %1%\Temp\
+@echo on
+for %%e in ("*") do copy /Y %%e "%base_path%\Temp\"
+@echo off
+rem "Go to the Temp directory to rename all the files to *.jpg"
+cd /D "%base_path%\Temp\"
 for %%f in ("*") do ren %%f %%f.jpg
-rem Move all the renamed files from the Temp directory to the Target directory
+rem "Move all the renamed files from the Temp directory to the Target directory"
+@echo on
 for %%g in ("*") do move %%g ../
 cd ..
 rmdir Temp
-rem TODO: Save the files to %USERPROFILE%/Pictures/ if no argument is passed
