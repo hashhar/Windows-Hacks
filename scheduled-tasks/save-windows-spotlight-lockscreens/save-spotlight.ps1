06dcccb05d43ba30a3c2dde348a79c018e3f4d12
Ashhar Hasan <hashhar_dev@outlook.com>
2017-02-05 01:00:08 +0530

    Segregate into the three categories

--- scheduled-tasks/save-windows-spotlight-lockscreens/save-spotlight.ps1
+++ scheduled-tasks/save-windows-spotlight-lockscreens/save-spotlight.ps1
@@ -1,8 +1,11 @@
 param (
-    [string]$path = "$($env:USERPROFILE)\Pictures"
+    # The location to save the images to.
+    [Parameter(Position=1,HelpMessage="The location to save the images to.")]
+    [string]
+    $SavePath = "$($env:USERPROFILE)\Pictures"
 )
 
-$Spotlight = "$path\Spotlight"
+$Spotlight = "$SavePath\Spotlight"
 $SpotlightAssets = "$Spotlight\CopyAssets"
 $SpotlightHorizontal = "$Spotlight\Desktop"
 $SpotlightVertical = "$Spotlight\Mobile"
@@ -14,18 +17,22 @@ New-Item "$SpotlightAssets" -ItemType directory -Force;
 New-Item "$SpotlightHorizontal" -ItemType directory -Force;
 New-Item "$SpotlightVertical" -ItemType directory -Force;
 
-foreach($file in (Get-Item "$SpotlightLocal\*"))
-{
-    if ((Get-Item $file).length -lt 100kb) { continue }
+foreach($file in (Get-Item "$SpotlightLocal\*")) {
+    if ((Get-Item $file).length -lt 100kb) {
+        continue 
+    }
     Copy-Item $file.FullName "$SpotlightAssets\$($file.Name).jpg";
 }
 
-foreach($newfile in (Get-Item "$SpotlightAssets\*"))
-{
+foreach($newfile in (Get-Item "$SpotlightAssets\*")) {
     $image = New-Object -comObject WIA.ImageFile;
     $image.LoadFile($newfile.FullName);
-    if($image.Width.ToString() -eq "1920"){ Move-Item $newfile.FullName "$SpotlightHorizontal" -Force; }
-    elseif($image.Width.ToString() -eq "1080"){ Move-Item $newfile.FullName "$SpotlightVertical" -Force; }
+    if($image.Width.ToString() -eq "1920") {
+        Move-Item $newfile.FullName "$SpotlightHorizontal" -Force; 
+    }
+    elseif($image.Width.ToString() -eq "1080") {
+        Move-Item $newfile.FullName "$SpotlightVertical" -Force; 
+    }
 }
 
 Remove-Item "$SpotlightAssets\*";
